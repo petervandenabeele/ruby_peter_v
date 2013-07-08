103458ed120796c22514da7b53d6beafe90ac585
Peter Vandenabeele <peter@vandenabeele.com>
2013-07-08 12:18:17 +0200

    Fix the truncate_utf8 so it also works on jruby
    * will report an issue on github.com/jruby/jruby with title:
      "test".encode("UTF-16BE", :invalid => :replace, :replace =>"") fails

--- lib/ruby_peter_v/truncate_utf8.rb
+++ lib/ruby_peter_v/truncate_utf8.rb
@@ -1,7 +1,11 @@
 class String
 
   def truncate_utf8(max_byte_size)
-    self.bytes.to_a[0...max_byte_size].pack('c*').force_encoding('UTF-8').encode("UTF-16BE", :invalid => :replace, :replace =>"").encode("UTF-8")
+    cut_off = self.bytes.to_a[0...max_byte_size].pack('c*').force_encoding('UTF-8')
+    while ! cut_off.valid_encoding? do
+      cut_off = cut_off[0..-2]
+    end
+    cut_off
   end
 
 end
