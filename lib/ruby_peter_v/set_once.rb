646cec1e58d355aaa952978a58072b7f890dc40e
Peter Vandenabeele <peter@vandenabeele.com>
2013-10-16 12:52:43 +0200

    Refactor, the #poodr way

--- lib/ruby_peter_v/set_once.rb
+++ lib/ruby_peter_v/set_once.rb
@@ -3,14 +3,24 @@ class RubyPeterV::SetOnceError < StandardError ; end
 class Object
 
   def set_once(attribute, value)
-    ivar_symbol = :"@#{attribute}"
+    ivar_symbol = ivar_symbol(attribute)
     instance_value = instance_variable_get(ivar_symbol)
-    if (instance_value && instance_value != value)
+    validate_immutable(attribute, instance_value, value)
+    instance_variable_set(ivar_symbol, value)
+  end
+
+private
+
+  def ivar_symbol(attribute)
+    :"@#{attribute}"
+  end
+
+  def validate_immutable(attribute, old_value, new_value)
+    if (old_value && old_value != new_value)
       raise(
         RubyPeterV::SetOnceError,
-        "Value of #{attribute} was #{instance_value}, trying to set it to #{value}")
+        "Value of #{attribute} was #{old_value}, trying to set it to #{new_value}")
     end
-    instance_variable_set(:"@#{attribute}", value)
   end
 
 end
